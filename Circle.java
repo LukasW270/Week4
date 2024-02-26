30f5bec71bdfa8f1bfa1f500d07b9eb9a3a3a69d
LukasW270 <lwitek270@gmail.com>
2024-02-26 14:28:59 -0500

    Updated

---
+++ Circle.java
@@ -0,0 +1,7 @@
+public class Circle {
+    public static void main(String[] args) {
+        double radius = 4.28;
+        double area = radius*radius*Math.PI;
+        System.out.println("The area of the circle is " + area + ".");
+    }
+}
